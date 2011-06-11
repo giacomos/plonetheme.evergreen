781b7cb739a39ec74adb61d84bd495dad733e216
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2011-06-11 19:28:32 +0000

    Update doc
    git-svn-id: http://svn.plone.org/svn/collective/plonetheme.evergreen/trunk@240801 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ import os
 setup(
         name='plonetheme.evergreen',
         description='An installable Diazo theme for Plone 4.1',
-        long_description=open('README.rst', 'rb').read(),
-        version='1.1dev',
+        long_description=open('README.rst', 'rb').read()+'\n'+
+                         open(os.path.join("docs", "INSTALL.txt")).read()+'\n'+
+                         open(os.path.join("docs", "HISTORY.txt")).read(),
+        version='1.0',
         author='Giacomo Spettoli',
         author_email='giacomo.spettoli@gmail.com',
         url='http://svn.plone.org/svn/collective/plonetheme.evergreen',
