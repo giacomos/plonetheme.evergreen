9be34ace75c7ea25d87b99254a2d13d51f35dfca
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2011-06-11 17:51:50 +0000

    Initial import.
    git-svn-id: http://svn.plone.org/svn/collective/plonetheme.evergreen/trunk@240796 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ README.rst
@@ -0,0 +1,6 @@
+Introduction
+============
+
+.. _`Evergreen`: http://www.freecsstemplates.org/preview/evergreen/
+.. _`plone.app.theming`: http://pypi.python.org/pypi/plone.app.theming
+.. _`Plone 4.1`: http://pypi.python.org/pypi/Plone/4.1rc2
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+        name='plonetheme.evergreen',
+        description='An installable Diazo theme for Plone 4.1',
+        long_description=open('README.rst', 'rb').read(),
+        version='0.1.0',
+        author='Giacomo Spettoli',
+        author_email='giacomo.spettoli@gmail.com',
+        url='https://github.com/giacomos/plonetheme.evergreen',
+        packages=find_packages(),
+        include_package_data=True,
+        namespace_packages=[
+            'plonetheme'
+            ],
+        install_requires=[
+            'setuptools',
+            'plone.app.theming',
+            ],
+        classifiers=[
+            "Framework :: Plone",
+            "Programming Language :: Python",
+            ],
+        entry_points={
+            'z3c.autoinclude.plugin': 'target = plone',
+            }
+        )
+
+
