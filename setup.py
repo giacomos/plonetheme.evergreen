bfec9a9bea5079d6a4ad6d5a98a49bb2f9fe2cf1
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2011-08-02 18:33:41 +0200

    Some fix

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 import os
 
+version='1.2'
+
 setup(
         name='plonetheme.evergreen',
         description='An installable Diazo theme for Plone 4.1',
         long_description=open('README.rst', 'rb').read()+'\n'+
                          open(os.path.join("docs", "INSTALL.txt")).read()+'\n'+
                          open(os.path.join("docs", "HISTORY.txt")).read(),
-        version='1.0',
+        version='1.2',
         author='Giacomo Spettoli',
         author_email='giacomo.spettoli@gmail.com',
         url='http://svn.plone.org/svn/collective/plonetheme.evergreen',
